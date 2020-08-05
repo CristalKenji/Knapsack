8b1e3ad04dca74c4b75aa5556f6c0c22a95799b5
Steffen <steffen.kohn@web.de>
2020-08-05 17:50:58 +0200

    Setup

---
+++ Knapsack/Item.cs
@@ -0,0 +1,44 @@
+﻿namespace Knapsack
+{
+    internal class Item
+    {
+        //private string name;
+        private uint _value;
+
+        private uint _weight;
+
+        public string Name { get; set; }
+
+        public uint Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value <= 0) throw new System.ArgumentException("Parameter cannot be 0", "Value");
+                _value = value;
+            }
+        }
+
+        public uint Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value <= 0) throw new System.ArgumentException("Parameter cannot be 0", "Weight");
+                _weight = value;
+            }
+        }
+
+        public Item(string name, uint value, uint weight)
+        {
+            Name = name;
+            Value = value;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return Name + "\tv: " + Value + "\tw: " + Weight;
+        }
+    }
+}
